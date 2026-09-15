0a47576c2641b289b50d8be9125839ba26093132
AZIZKH7 <136383689+AZIZKH7@users.noreply.github.com>
2026-09-15 14:55:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1948,7 +1948,7 @@ if not st.session_state.authorized and checkout_mode:
 </html>
 """
 
-    st.iframe(checkout_html, height=650, scrolling=False)
+    st.iframe(checkout_html, height=650 )
     st.stop()
 
 
